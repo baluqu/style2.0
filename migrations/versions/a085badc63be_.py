2bdf106495448e901ad739e39114f30d083bc293
baluqu <brazeeali7@gmail.com>
2026-05-13 09:40:14 +0300

    Merge migration heads and finalize DB schema for persistence layer

---
+++ migrations/versions/a085badc63be_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a085badc63be
+Revises: 25a78fd6663d, a3df1b67c2d4
+Create Date: 2026-05-13 09:34:22.128388
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a085badc63be'
+down_revision = ('25a78fd6663d', 'a3df1b67c2d4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
